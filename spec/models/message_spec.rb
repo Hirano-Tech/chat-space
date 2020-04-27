ad67c74dec9db8ac098c165e3b07fcda898839d8
Hirano-Tech <Information@Hirano.cfbx.jp>
2020-04-27 14:28:04 +0900

    Test the posting function.

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -19,18 +19,17 @@ RSpec.describe Message, type: :model do
     context 'messageを保存できない場合' do
       it ' content と imageが両方空だと保存できないこと' do
         message = build(:message, content: nil, image: nil)
-        
         message.valid?
         expect(message.errors[:content]).to include("を入力してください")
       end
 
-      it 'group_idがないと保存できないこと' do
+      it 'group_idが無いと保存できないこと' do
         message = build(:message, group_id: nil)
         message.valid?
         expect(message.errors[:group]).to include("を入力してください")
       end
 
-      it 'user_idがないと保存できないこと' do
+      it ' user_idが無いと保存できないこと' do
         message = build(:message, user_id: nil)
         message.valid?
         expect(message.errors[:user]).to include("を入力してください")
