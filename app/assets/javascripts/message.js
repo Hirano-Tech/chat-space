acf26c9710135c3f032ef6c128d688abf322e9aa
Hirano-Tech <Information@Hirano.cfbx.jp>
2020-04-28 20:38:06 +0900

    Receive the returned JSON with the done method and create the HTML.

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,46 @@
 $(function(){
-  $('#new_message').on('submit', function(e){
+
+  function buildHTML(message){
+    if ( message.image ) {
+      var html = 
+        `<div class="contents">
+          <div class="contents__name">
+            <div class="contents__name__name">
+              ${message.user_id}
+            </div>
+            <div class="contents__name__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="message">
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
+          </div>
+          <img src=${message.image} >
+        </div>`
+      return html;
+    } else {
+      var html =
+        `<div class="contents">
+        <div class="contents__name">
+          <div class="contents__name__name">
+            ${message.user_id}
+          </div>
+          <div class="contents__name__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="message">
+          <p class="lower-message__content">
+            ${message.content}
+          </p>
+        </div>`
+      console.log();
+      return html;
+    };
+  }
+$('#new_message').on('submit', function(e){
     e.preventDefault()
     var formData = new FormData(this);
     var url = $(this).attr('action');
@@ -11,5 +52,8 @@ $(function(){
       processData: false,
       contentType: false
     })
-  })
+      .done(function(data){
+        var html = buildHTML(data);
+      })
 })
+});
